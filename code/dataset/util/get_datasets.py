5c2b8373860c180cd2cfae8c72351751934ea370
innocent-oss <71064498+innocent-oss@users.noreply.github.com>
2022-10-03 11:47:22 +0500

    ./genetic

--- code/dataset/util/get_datasets.py
+++ code/dataset/util/get_datasets.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append(r'/content/drive/MyDrive/Genetic-U-Net-main(1)/Genetic-U-Net-main/code/train/train_model.py')
+sys.path.append(r'./genetic/code/train/train_model.py')
 def get_datasets(dataset_name, data_root, train, transforms=None):
     if dataset_name == 'DRIVE':
-        sys.path.append('/content/drive/MyDrive/Genetic-U-Net-main(1)/Genetic-U-Net-main/code/dataset')
+        sys.path.append('./genetic/code/dataset')
         from DRIVE_dataset import DRIVE_dataset
         dataset = DRIVE_dataset(data_root=data_root, train=train, transforms=transforms)
         num_return = dataset.num_return
